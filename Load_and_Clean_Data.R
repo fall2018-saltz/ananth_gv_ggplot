a890b4821f87af4b9ac27614b0466f66188c542e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 13:38:58 -0400

    Saving Load_and_Clean_Data.R

--- Load_and_Clean_Data.R
+++ Load_and_Clean_Data.R
@@ -1,5 +1,5 @@
 
         # function defination
   
-  data <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors=FALSE)      # read.csv() function to read the data set. url() to direct the data source.
+  resultant_dataframe <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors=FALSE)      # read.csv() function to read the data set. url() to direct the data source.
   
