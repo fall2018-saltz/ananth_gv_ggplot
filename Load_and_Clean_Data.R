06b26e0dd71a2db1d01261787c4a5c5f4a9361dd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 13:38:21 -0400

    Saving Load_and_Clean_Data.R

--- Load_and_Clean_Data.R
+++ Load_and_Clean_Data.R
@@ -2,17 +2,3 @@
         # function defination
   
   data <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors=FALSE)      # read.csv() function to read the data set. url() to direct the data source.
-  length_data <- nrow(data)                                                                                                                               #Compute length using nrow to get index of last row.
-  data <- data[-length_data, ]
-  data <- data[-1, ]                                   # Remove first row which has USA
-  nrow(data)                             # Displays the length of modified data frame = 51
-  
-  
-  #	Make sure there are precisely 4 columns, with the following names - stateName, population, popOver18, percentOver18. 
-  colnames(data)                         # use colnames to display column name metadata
-  data$SUMLEV <- NULL                    # set other column name to NULL to be omitted
-  data$REGION <- NULL                    # set other column name to NULL to be omitted
-  data$STATE <- NULL                      # set other column name to NULL to be omitted
-  colnames(data)
-                         # return clean data frame.
-resultant_dataframe <- data
