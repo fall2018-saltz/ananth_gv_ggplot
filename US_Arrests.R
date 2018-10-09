b5d95ed823d990be9d829e8ce2071311f2d57c08
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 19:41:25 -0400

    Saving US_Arrests.R

--- US_Arrests.R
+++ US_Arrests.R
@@ -1,3 +1,4 @@
 
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
+
 arrests <- USArrests         #Assigning the US USArrests data to arrests variable. This is fed as input to next R module.
