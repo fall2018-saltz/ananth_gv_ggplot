cb35aa1a088bf3ce7a83bb130a44a2a9391b51d7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 19:40:58 -0400

    Saving Merged_Data.R

--- Merged_Data.R
+++ Merged_Data.R
@@ -1,6 +1,8 @@
 
 
-#3)	Create a merged dataframe -- with the attributes from both dataframesmerged_data <- merge(us_arrest, us_census, by.x = "row.names", by.y = c("NAME"), all = TRUE)       # Merge data from us_census and us_arrest got as input from previous     
+#3)	Create a merged dataframe -- with the attributes from both dataframes
+
+merged_data <- merge(us_arrest, us_census, by.x = "row.names", by.y = c("NAME"), all = TRUE)       # Merge data from us_census and us_arrest got as input from previous     
                                                                                                 # modules using merge function on top of row.name and State Name.
 
 
