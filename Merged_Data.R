8a746fce4e505d6ceb85925bbf5ea755d9ee7228
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 17:49:21 -0400

    Saving Merged_Data.R

--- Merged_Data.R
+++ Merged_Data.R
@@ -17,6 +17,6 @@ plot_murder_rate <- qplot(merged_data$Murder, geom="histogram",
       xlim = c(0, 20))
       
       
-population_box_plot < - ggplot(data = merged_data, aes(x = "Population", y = merged_data$POPESTIMATE2017)) + 
+population_box_plot <- ggplot(data = merged_data, aes(x = "Population", y = merged_data$POPESTIMATE2017)) + 
   geom_boxplot() # I set the y axis scale so the plot looks better
 
