ae1b321f2d18286f87a0531f7fb2916ceeb10d11
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 19:38:58 -0400

    Saving Merged_Data.R

--- Merged_Data.R
+++ Merged_Data.R
@@ -1,8 +1,6 @@
 
 merged_data <- merge(us_arrest, us_census, by.x = "row.names", by.y = c("NAME"), all = TRUE)       # Merge data from us_census and us_arrest got as input from previous     
-
-merged_data
-
+                                                                                                # modules using merge function on top of row.name and State Name.
 
 library(ggplot2)
 plot_population <- qplot(merged_data$POPESTIMATE2017, geom="histogram")
