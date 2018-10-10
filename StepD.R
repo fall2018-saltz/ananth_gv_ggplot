e332b180be049d35e2e08dc8d04ab595168eb854
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 20:34:15 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -3,6 +3,7 @@
 
 #12)Generate a scatter plot – have population on the X axis, the percent over 18 on the y axis, and the size & color represent the murder rate
 
+library(ggplot2) 
 scatter_plot <- ggplot(merged_data, aes(x=merged_data$POPESTIMATE2017, y=merged_data$PCNT_POPEST18PLUS, size=merged_data$Murder)) +
   geom_point(shape=19, fill="blue", color="darkred") + ggtitle("Explore Murders") +
   labs(x="Population",y="Percentage of Population above 18") + 
