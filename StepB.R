b1ff5dbef90b1feb6b39c86894ef5b413877fc08
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:51:53 -0400

    Saving StepB.R

--- StepB.R
+++ StepB.R
@@ -17,7 +17,7 @@ plot_rape <- qplot(merged_data$Rape, geom="histogram")                          
 
 
 # The Parameters to be adjusted for other histograms to look fine are to add attributes that make the histogram readable. Currently the histogram is not distributed in the right way 
-# due to bucket sizing and range. By adding the labels, bandwidth, fill, title, range and color- histograms are better represented as below.
+# due to bucket sizing, bin width and range. By adding the labels, bandwidth, fill, title, range and color- histograms are better represented as below.
 
 plot_murder_rate_labelled <- qplot(merged_data$Murder, geom="histogram",
                                    bandwidth = 0.5,                                                                       
