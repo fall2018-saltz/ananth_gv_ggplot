8382c9ea2ad61f2b8dc58c7148f836cfb7ae7e03
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 00:07:47 -0400

    Saving StepC.R

--- StepC.R
+++ StepC.R
@@ -9,23 +9,24 @@ library(dplyr)                                                   # use library d
 df1 <- select(merged_data, POPESTIMATE2017)                      # select function to select the required column from the dataframe.
 df2 <- (merged_data$POPESTIMATE2017/100000)*merged_data$Murder   # calculate the murder per state - Given murder per 100000 population, Number of Murders per state is calculated by dividing state population by 100000 and multiplying by the corresponding murder rate of the state.
 
+# result is stored in the dataframe df2
 
-merged_data$NumberofMurders <- round(df2, digits = 0)
-
+merged_data$NumberofMurders <- round(df2, digits = 0)            # number of murders for every state is merged with original datframe creating new column NumberofMurders
+                                                                 # round is used to round the number of murders to whole number.
 #8)	Generate a bar chart, with the number of murders per state
 
-bar_chart_murder_state <- ggplot() + geom_col(data = merged_data, aes(x = factor(Row.names), y = merged_data$NumberofMurders)) +       #use ggplot and geom_col fucntion to create bar chart. Pass dataframe and declare x and y axis variables
+bar_chart_murder_state <- ggplot() + geom_col(data = merged_data, aes(x = factor(Row.names), y = merged_data$NumberofMurders)) +       #use ggplot and geom_col fucntion to create bar chart. Pass dataframe and declare x and y axis variables namely State names and Number of Murders.
   ggtitle("Total Murders") +            #Use ggtitle to declare title of the chart
-  labs(x="State",y="Murder rate") +     # lables to be displayed on chart using labs attribute
+  labs(x="State",y="Murders") +     # lables to be displayed on chart using labs attribute
   theme(plot.title = element_text(color="#666666", face="bold", size=15, hjust=0)) +   #theme and font in which the titles, axis lables should be displayed.
-  theme(axis.title = element_text(color="#666666", face="bold", size=15))
+  theme(axis.title = element_text(color="#666666", face="bold", size=15))  # color and font of the labels are set using attributes face, size, color
 
 
 #9)	Generate a bar chart, with the number of murders per state. Rotate text (on the X axis), so we can see x labels, also add a title named “Total Murders”.
 
 bar_chart_murder_state_axis_vertical <- ggplot() + geom_col(data = merged_data, aes(x = factor(Row.names), y = merged_data$NumberofMurders)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Total Murders") +            # element_text attribute is used to orient lables angle by 90 degrees vertical to the x axis.
-  labs(x="State",y="Murder rate") + 
+  labs(x="State",y="Murders") + 
   theme(plot.title = element_text(color="#666666", face="bold", size=15, hjust=0)) +
   theme(axis.title = element_text(color="#666666", face="bold", size=15))
 
@@ -34,7 +35,7 @@ bar_chart_murder_state_axis_vertical <- ggplot() + geom_col(data = merged_data, 
 
 bar_chart_murder_state_sorted <- ggplot() + geom_col(data = merged_data, aes(x = reorder(factor(Row.names), -merged_data$NumberofMurders, sum), y = merged_data$NumberofMurders)) +   # reorder attribute is used to sort the graph or chart in ascending or descending. -merged_data$Murder, sum parameters are used where the - indicates the graph is order in descending starting from 0 in x axis by murder rate.
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Total Murders") +
-  labs(x="State",y="Murder rate") + 
+  labs(x="State",y="Murders") + 
   theme(plot.title = element_text(color="#666666", face="bold", size=15, hjust=0)) +
   theme(axis.title = element_text(color="#666666", face="bold", size=15))
 
