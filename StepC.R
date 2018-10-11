9050d69e0c01e2c90beb09bc9d89fac9b25f28b3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:58:20 -0400

    Saving StepC.R

--- StepC.R
+++ StepC.R
@@ -33,7 +33,7 @@ bar_chart_murder_state_axis_vertical <- ggplot() + geom_col(data = merged_data, 
 
 #10) Generate a new bar chart, the same as in the previous step, but also sort the x-axis by the murder rate
 
-bar_chart_murder_state_sorted <- ggplot() + geom_col(data = merged_data, aes(x = reorder(factor(Row.names), -merged_data$NumberofMurders, sum), y = merged_data$Murder)) +   # reorder attribute is used to sort the graph or chart in ascending or descending. -merged_data$Murder, sum parameters are used where the - indicates the graph is order in descending starting from 0 in x axis by murder rate.
+bar_chart_murder_state_sorted <- ggplot() + geom_col(data = merged_data, aes(x = reorder(factor(Row.names), -merged_data$NumberofMurders, sum), y = merged_data$NumberofMurders)) +   # reorder attribute is used to sort the graph or chart in ascending or descending. -merged_data$Murder, sum parameters are used where the - indicates the graph is order in descending starting from 0 in x axis by murder rate.
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Total Murders") +
   labs(x="State",y="Murder rate") + 
   theme(plot.title = element_text(color="#666666", face="bold", size=15, hjust=0)) +
@@ -42,7 +42,7 @@ bar_chart_murder_state_sorted <- ggplot() + geom_col(data = merged_data, aes(x =
 
 #11) Generate a third bar chart, the same as the previous step, but also showing percentOver18 as the color of the bar
 
-bar_chart_murder_state_filled <- ggplot() + geom_col(data = merged_data, aes(x = reorder(factor(Row.names), -merged_data$NumberofMurders, sum), y = merged_data$Murder, fill=merged_data$PCNT_POPEST18PLUS)) +  # fill attribute is used to have the bar chart color filled by Percentage of population over 18. the color gradient varies by the magnitude of value of percentage.
+bar_chart_murder_state_filled <- ggplot() + geom_col(data = merged_data, aes(x = reorder(factor(Row.names), -merged_data$NumberofMurders, sum), y = merged_data$NumberofMurders, fill=merged_data$PCNT_POPEST18PLUS)) +  # fill attribute is used to have the bar chart color filled by Percentage of population over 18. the color gradient varies by the magnitude of value of percentage.
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Total Murders") +
   labs(x="State",y="Murder rate") + 
   theme(plot.title = element_text(color="#666666", face="bold", size=15, hjust=0)) +
